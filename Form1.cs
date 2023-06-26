1a476593ed15f5aa368a3eb838a40c32a442699d
Luc Bigand <lucbigand@yahoo.fr>
2023-06-26 11:35:08 +0200

    Selected file has to be a CSV file

--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,7 @@ namespace FileReaderCS
         private void selectButton_Click(object sender, EventArgs e)
         {
             OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "CSV files (*.csv)|*.csv";
             if (fileDialog.ShowDialog() == DialogResult.OK) { 
                 filename = fileDialog.FileName; 
             }

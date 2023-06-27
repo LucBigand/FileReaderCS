ad8b3ffb03169e92cd644edf4ed94f086bfa16a6
Luc Bigand <lucbigand@yahoo.fr>
2023-06-27 11:09:02 +0200

    Take into account possibility of not every field being informed

--- Client.cs
+++ Client.cs
@@ -11,11 +11,33 @@ namespace FileReaderCS
         public Client(string csvLine, char separator, Dictionary<string, int> columnOrder)
         {
             string[] entry = csvLine.Split(separator);
-            title = entry[columnOrder[columnNames[0]]];
-            lastName = entry[columnOrder[columnNames[1]]];
-            firstName = entry[columnOrder[columnNames[2]]];
-            postalCode = entry[columnOrder[columnNames[3]]];
-            town = entry[columnOrder[columnNames[4]]];
+            title = GetFieldValue(entry, columnOrder, 0);
+            lastName = GetFieldValue(entry, columnOrder, 1);
+            firstName = GetFieldValue(entry, columnOrder, 2);
+            postalCode = GetFieldValue(entry, columnOrder, 3);
+            town = GetFieldValue(entry, columnOrder, 4);
+        }
+
+        /*
+         * Return from the entry the value of what would be the field of index i in the default
+         * column order as defined in columnNames
+         * Parameters :
+         *      entry : The entry informations as a string array (possibly not in the default 
+         *              column order)
+         *      columnOrder : the order in which the informations are presented in the entry, as
+         *              a dictionary that associates the column name with its postion
+         *      i : the index of the field whose value is to be returned
+         */
+        private string GetFieldValue(string[] entry, Dictionary<string, int> columnOrder, int i)
+        {
+            if (entry.Length < i)
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return entry[columnOrder[columnNames[i]]];
+            }
         }
 
     }

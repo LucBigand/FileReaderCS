747a679251be9f8c4ae2cbd2c7c8016665cc7694
Luc Bigand <lucbigand@yahoo.fr>
2023-06-27 10:38:24 +0200

    Make Client attributes readonly instead of using getters

--- Client.cs
+++ Client.cs
@@ -1,44 +1,21 @@
-﻿namespace FileReaderCS
+﻿using System.Drawing.Text;
+
+namespace FileReaderCS
 {
     internal class Client
     {
 
-        private string title, lastName, firstName, postalCode, town;
+        internal readonly string title, lastName, firstName, postalCode, town = "";
         internal static readonly string[] columnNames = { "CIV_LIBELLE", "NOM", "PRENOM", "CP", "VILLE" };
 
         public Client(string csvLine, char separator, Dictionary<string, int> columnOrder)
         {
-            string[] data = csvLine.Split(separator);
-            title = data[columnOrder[columnNames[0]]];
-            lastName = data[columnOrder[columnNames[1]]];
-            firstName = data[columnOrder[columnNames[2]]];
-            postalCode = data[columnOrder[columnNames[3]]];
-            town = data[columnOrder[columnNames[4]]];
-        }
-
-        public string getTitle()
-        {
-            return title;
-        }
-
-        public string getLastName()
-        {
-            return lastName;
-        }
-
-        public string getFirstName()
-        {
-            return firstName;
-        }
-
-        public string getPostalCode()
-        {
-            return postalCode;
-        }
-
-        public string getTown()
-        {
-            return town;
+            string[] entry = csvLine.Split(separator);
+            title = entry[columnOrder[columnNames[0]]];
+            lastName = entry[columnOrder[columnNames[1]]];
+            firstName = entry[columnOrder[columnNames[2]]];
+            postalCode = entry[columnOrder[columnNames[3]]];
+            town = entry[columnOrder[columnNames[4]]];
         }
 
     }
